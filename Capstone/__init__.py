a0f62ba33d3382b58718421028d00449ea15c8cd
j-chavez-sw <68924976+j-chavez-sw@users.noreply.github.com>
2021-05-17 21:02:45 -0700

    desktop recommit test deployment  5/17

--- Capstone/__init__.py
+++ Capstone/__init__.py
@@ -8,12 +8,45 @@ app = Flask(__name__, template_folder='Templates')
 app.config['SECRET_KEY'] = 'mysecret'
 #
 #######DATABASE###########
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#
+# db = SQLAlchemy(app)
+# Migrate(app, db)
+##########################
+
+#######DATABASE2###########
+URI = "postgresql://kszehnilxhvbqp:f24a461e28fb2fe47d5b620d87c7375ffa34cba7f4e24d2cdfa50fdbcef9100b@ec2-184-73-198-174.compute-1.amazonaws.com:5432/datc0rmuf010ql"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app, db)
+# from sqlalchemy import create_engine
+#
+# engine = create_engine(URI)
+#
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import Column, Integer, String, Date
+#
+# Base = declarative_base()
+#
+# class User(Base):
+#
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(50), unique=True, index=True)
+#     username = db.Column(db.String(50), unique=True, index=True)
+#     password_hash = db.Column(db.String(128))
+#
+#
+#     def __repr__(self):
+#         return f"Username {self.username}"
+#
+# Base.metadata.create_all(engine)
+
 ##########################
 
 ##########LOGIN###########
